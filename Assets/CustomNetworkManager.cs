c41aa8d4aa8e54fdab8446aa272c100442e65f74
saszer <43649755+saszer@users.noreply.github.com>
2020-10-18 10:38:43 +1100

    Working Loop
    Removed lobby: custom scene change/network manager
    UI checks
    Ability Check

---
+++ Assets/CustomNetworkManager.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Networking.NetworkSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine;
+public class CustomNetworkManager : NetworkManager
+{
+    public PrizeMaster prizemastertoupdateui;
+    public PlayerListUi playerlistui;
+    public QueUIControl quicntrl;
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        if (!clientLoadedScene)
+        {
+            // Ready/AddPlayer is usually triggered by a scene load completing. if no scene was loaded, then Ready/AddPlayer it here instead.
+            ClientScene.Ready(conn);
+            //  if (m_AutoCreatePlayer)
+            // {
+            ClientScene.AddPlayer(0);
+            // }
+        }
+
+        string loadedSceneName = SceneManager.GetSceneAt(0).name;
+        SceneManager.LoadScene(loadedSceneName);
+
+        prizemastertoupdateui.CmdPreviousGamewinnerUiUpdate();
+       // playerlistui.Cmdupdateui();
+
+        if (loadedSceneName == "Lobby")
+        {
+            quicntrl.Lobby();
+        }
+        else
+        {
+            quicntrl.LocalDisableEnableAfterQue();
+        } 
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        StopClient();
+        if (conn.lastError != NetworkError.Ok)
+        {
+            if (LogFilter.logError) { Debug.LogError("ClientDisconnected due to error: " + conn.lastError); }
+        }
+    }
+
+
+    /*   public virtual void OnLobbyClientEnter()
+        {
+            string loadedSceneName = SceneManager.GetSceneAt(0).name;
+            SceneManager.LoadScene(loadedSceneName);
+        } */
+}
+
--- Assets/Scripts Teir3/ClientServerStartUIConnection.cs
+++ Assets/Scripts Teir3/ClientServerStartUIConnection.cs
@@ -9,7 +9,7 @@ public class ClientServerStartUIConnection : NetworkBehaviour
 
     public GetLatestBlockVanillaNethereum getlatestBlockVanillaNethereum;
 
-    bool questarted;
+    public bool questarted;
 
     private void Start()
     {
@@ -32,6 +32,6 @@ public class ClientServerStartUIConnection : NetworkBehaviour
     [ClientRpc]
     public void RpcDisableClickButton()
     {
-        Destroy(StartButton);
+        StartButton.SetActive(false);
     }
 }
